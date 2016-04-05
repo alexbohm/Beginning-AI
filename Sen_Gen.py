9f2acf02f62e234349975c8b7e57e8001bde0557
Alex Bohm <aabohm7@gmail.com>
2016-04-05 17:10:22 -0500

    Cleaned up some code

--- Sen_Gen.py
+++ Sen_Gen.py
@@ -11,25 +11,22 @@ class Sen_Gen(object):
 		for a in range(number):
 			word = ""
 			word = str(raw_input("Word: "))
-			while word in self.all_words:
+			while word in self.all_words: #check if word is already in list
 				if word in self.all_words:
 					print "already in list"
 				word = str(raw_input("Word: "))
 			word_type = ""
 			word_type = str(raw_input("Word Type: "))
-			while word_type not in self.types:
+			while word_type not in self.types: #check if word type is valid
 				if word_type not in self.types:
 					print "invalid type"
 				word_type = str(raw_input("Word Type: "))
-			self.types[word_type].append(word)
+			self.types[word_type].append(word) #add word to it's list
+			self.all_words.append(word) #add word to full list
 			with open("%s.txt" % (word_type), "a") as f:
-				f.write("%s\n" %(word))
-			self.all_words.append(word)
+				f.write("%s\n" % (word)) #add word to appropriate file
 	def gen_sen(self, lower, upper):
 		self.random = {}
 		for a in range(5):
 			self.random[a] = randrange(lower, upper)
 		return "%s %s the %s %s" % (self.types["noun"][self.random[1]], self.types["verb"][self.random[2]], self.types["adjective"][self.random[3]], self.types["noun"][self.random[4]])
-sen = Sen_Gen()
-for a in range(10):
-	print sen.gen_sen(0, 900)
