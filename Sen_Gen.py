700db3ecb0b13f646fa3b957a94eb3f033e227a3
Alex Bohm <aabohm7@gmail.com>
2016-04-08 19:18:41 -0500

    Added files via upload

--- Sen_Gen.py
+++ Sen_Gen.py
@@ -1,89 +1,42 @@
 from random import randrange
-all_tags = []
-all_verbs = []
-all_words = [] #not verbs
-all_types = {"noun":[], "adjective":[], "adverb":[], "conjunction":[], "preposition":[], "interjection":[]}
-
 class Word(object):
-	def __init__(self, word,w_type,tags = {},forms = {}):
+	def __init__(self, word,w_type,forms = {}):
 		self.word = word
 		self.type = w_type
-		self.tags = tags
 		self.forms = forms
 	def save(self):
-		return "%s:%s:%s:%s" % (self.word, self.type, str(self.tags), str(self.forms))
-
-class Verb(object):
-	def __init__(self, infinitive, past, present, future, tag):
-		self.infinitive = infinitive
-		self.past = past
-		self.present = present
-		self.future = future
-		self.tag = tag
-
-def add_tag(tag):
-	all_tags.append(tag)
-
-def make_word(word, type_):
-	if type_!="verb":
-		if word not in all_words:
-			all_words.append(Word(word, type_))
-	elif type_ == "verb":
-		infinitive = raw_input("Infinitive: ")
-		past = raw_input("Past Tense: ")
-		present = raw_input("Present Tense: ")
-		future = raw_input("Future Tense: ")
-		tag = raw_input("Tag: ")
-		if tag not in all_tags:
-			all_tags.append(tag)
-		if verb not in all_verbs:
-			all_verbs.append(Verb(infinitive, past, present, future, tag))
-	else:
-		print "Sorry, you mispelled the type."
+		return "%s:%s:%s\n" % (self.word, self.type, str(self.forms))
 
 class Sen_Gen(object):
-	def __init__(self, structure=[]):
-		self.structure = structure
-		for e in all_types:
-			with open("%s.txt" % (e), "r") as f:
-				all_types[e] = f.read().splitlines()
-		with open("%s.txt" % ("verb"), "a") as f:
-			lines = f.read().splitlines()
-			for e in lines:
-				verb_parts = e.split(":")
-				all_verbs.append(Verb(verb_parts[0], verb_parts[1], verb_parts[2], verb_parts[3], verb_parts[4], verb_parts[5]))
-	def add_words(self, amount):
-		i = 0
-		while True:
-			word = raw_input("Word: ") #this program takes it for granted that the user does not make typos or repeats
-			type_ = raw_input("Type: ")
-			i+=1
-			if type_ == "verb":
-				blue = make_word(word, type_, tag)
-			else:
-				blue = make_word(word, type_)
-			if i > amount-1:
-				break
-			if type_!="verb":
-				for type_w in all_types:
-					if type_ == type_w:
-						all_types[type_w].append()
-						with open("%s.txt" % (type_w), "a") as f:
-							f.write("%s\n" %(blue))
-			else:
-				with open("%s.txt" % ("verb"), "a") as f:
-					f.write("%s:%s:%s:%s:%s\n" % (blue.infinitive, blue.past, blue.present, blue.future, blue.tag))
-	def make_sen(self, amount=3):
-		i = 0
-		stc = ""
-		senl = []
-		while i<amount:
-			for e in range(0, len(self.structure)): //one of elements loaded in constructor containing strings/keys
-				types = all_types[self.structure[e]]
-				stc += " " + types[5] #make a random number here
-			senl.append(stc)
-			i+=1
-		return senl
+	def __init__(self):
+		self.words = {"noun":{}, "adjective":{}, "verb":{}, "adverb":{}, "conjunction":{}, "preposition":{}, "interjection":{}}
+		with open("words.txt", "r") as f:
+			temp = f.read().splitlines()
+		for word in temp:
+			temp2 = word.split(":")
+			self.words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1], eval(temp2[2]))
+	def add_word(self):
+		w_type = str(raw_input("Word Type: "))
+		while w_type not in self.words: #check if word type is valid
+			if w_type not in self.words:
+				print "invalid type"
+			w_type = str(raw_input("Word Type: "))
+		if w_type == "verb":
+			forms = {}
+			forms['infinitive'] = raw_input("Infinitive: ")
+			forms['past'] = raw_input("Past Tense: ")
+			forms['present'] = raw_input("Present Tense: ")
+			forms['future'] = raw_input("Future Tense: ")
+		else:
+			word = str(raw_input("Word: "))
+			while word in self.words[w_type]: #check if word is valid
+				if word in self.words[w_type]:
+					print "Aready in List"
+				word = str(raw_input("Word: "))
+			self.words[w_type][word] = Word(word, w_type)
+		with open("words.txt", "a") as f:
+			f.write(self.words[w_type][word].save())
+		
 
 
 
@@ -96,3 +49,58 @@ class Sen_Gen(object):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''class Sen_Gen(object):
+	def __init__(self):
+		self.all_words = []
+		self.types = {"noun":[], "adjective":[], "verb":[], "adverb":[], "conjunction":[], "preposition":[], "interjection":[]}
+		for type_w in self.types: #read in all words from files
+			with open("%s.txt" % (type_w), "r") as f:
+				self.types[type_w] = f.read().splitlines()
+			self.all_words += self.types[type_w]
+	def add_words(self, number):
+		for a in range(number):
+			word = ""
+			word = str(raw_input("Word: "))
+			while word in self.all_words: #check if word is already in list
+				if word in self.all_words:
+					print "already in list"
+				word = str(raw_input("Word: "))
+			word_type = ""
+			word_type = str(raw_input("Word Type: "))
+			while word_type not in self.types: #check if word type is valid
+				if word_type not in self.types:
+					print "invalid type"
+				word_type = str(raw_input("Word Type: "))
+			self.types[word_type].append(word) #add word to it's list
+			self.all_words.append(word) #add word to full list
+			with open("%s.txt" % (word_type), "a") as f:
+				f.write("%s\n" % (word)) #add word to appropriate file
+	def gen_sen(self, lower, upper):
+		self.random = {}
+		for a in range(5):
+			self.random[a] = randrange(lower, upper)
+		return "%s %s the %s %s" % (self.types["noun"][self.random[1]], self.types["verb"][self.random[2]], self.types["adjective"][self.random[3]], self.types["noun"][self.random[4]])'''
