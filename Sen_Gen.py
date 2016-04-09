a53580ff2d386c85081026a372f01183b404aef7
Alex Bohm <aabohm7@gmail.com>
2016-04-09 09:30:39 -0500

    Added files via upload

--- Sen_Gen.py
+++ Sen_Gen.py
@@ -3,9 +3,13 @@ class Word(object):
 	def __init__(self, word,w_type,forms = {}):
 		self.word = word
 		self.type = w_type
-		self.forms = forms
+		if w_type == "verb":
+			self.forms = forms
 	def save(self):
-		return "%s:%s:%s\n" % (self.word, self.type, str(self.forms))
+		if self.type == "verb":
+			return "%s|%s|%s\n" % (self.word, self.type, str(self.forms))
+		else:
+			return "%s|%s\n" % (self.word, self.type)
 
 class Sen_Gen(object):
 	def __init__(self):
@@ -13,94 +17,35 @@ class Sen_Gen(object):
 		with open("words.txt", "r") as f:
 			temp = f.read().splitlines()
 		for word in temp:
-			temp2 = word.split(":")
-			self.words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1], eval(temp2[2]))
+			temp2 = word.split("|")
+			if temp2[1]=="verb":
+				self.words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1], eval(temp2[2]))
+			else:
+				self.words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1])
 	def add_word(self):
 		w_type = str(raw_input("Word Type: "))
 		while w_type not in self.words: #check if word type is valid
 			if w_type not in self.words:
 				print "invalid type"
 			w_type = str(raw_input("Word Type: "))
+		forms = {}
 		if w_type == "verb":
 			forms = {}
 			forms['infinitive'] = raw_input("Infinitive: ")
+			while forms['infinitive'] in self.words[w_type]: #check if word is valid
+				if forms['infinitive'] in self.words[w_type]:
+					print "Verb Aready in List"
+				forms['infinitive'] = raw_input("Infinitive: ")
 			forms['past'] = raw_input("Past Tense: ")
 			forms['present'] = raw_input("Present Tense: ")
 			forms['future'] = raw_input("Future Tense: ")
+			word = forms['infinitive']
 		else:
 			word = str(raw_input("Word: "))
 			while word in self.words[w_type]: #check if word is valid
 				if word in self.words[w_type]:
 					print "Aready in List"
 				word = str(raw_input("Word: "))
-			self.words[w_type][word] = Word(word, w_type)
+		self.words[w_type][word] = Word(word, w_type, forms)
 		with open("words.txt", "a") as f:
 			f.write(self.words[w_type][word].save())
-		
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''class Sen_Gen(object):
-	def __init__(self):
-		self.all_words = []
-		self.types = {"noun":[], "adjective":[], "verb":[], "adverb":[], "conjunction":[], "preposition":[], "interjection":[]}
-		for type_w in self.types: #read in all words from files
-			with open("%s.txt" % (type_w), "r") as f:
-				self.types[type_w] = f.read().splitlines()
-			self.all_words += self.types[type_w]
-	def add_words(self, number):
-		for a in range(number):
-			word = ""
-			word = str(raw_input("Word: "))
-			while word in self.all_words: #check if word is already in list
-				if word in self.all_words:
-					print "already in list"
-				word = str(raw_input("Word: "))
-			word_type = ""
-			word_type = str(raw_input("Word Type: "))
-			while word_type not in self.types: #check if word type is valid
-				if word_type not in self.types:
-					print "invalid type"
-				word_type = str(raw_input("Word Type: "))
-			self.types[word_type].append(word) #add word to it's list
-			self.all_words.append(word) #add word to full list
-			with open("%s.txt" % (word_type), "a") as f:
-				f.write("%s\n" % (word)) #add word to appropriate file
-	def gen_sen(self, lower, upper):
-		self.random = {}
-		for a in range(5):
-			self.random[a] = randrange(lower, upper)
-		return "%s %s the %s %s" % (self.types["noun"][self.random[1]], self.types["verb"][self.random[2]], self.types["adjective"][self.random[3]], self.types["noun"][self.random[4]])'''
