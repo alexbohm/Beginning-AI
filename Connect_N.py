5842f83d40a4350a1a091d68bbd4bff0ba1f95ac
Linus <athenuslabs@users.noreply.github.com>
2016-04-11 16:35:43 -0500

    Added links and less bugs
    Now works correctly even when no nodes work with other nodes.

--- Connect_N.py
+++ Connect_N.py
@@ -5,7 +5,7 @@ class Link (object):
 		self.node2 = node2 #word2
 		self.cd = charge_deduc #the amount of charge deducted from initial
 	def charge_reduc(self, charge):
-		return charge - self.cd #charge only goes 10 links
+		return (charge - self.cd) #charge only goes 10 links
 		#that way, if the starting word of the network is "ridiculous", it doesn't end up on the other end
 		#of the network saying "mommy"
 
@@ -20,17 +20,19 @@ class Network(object):
 		crawled, self.path = [], [] #crawled will become useful later
 		for e in links:				#path is the sentence
 			if e.node1==word1:
-				good_link = e
+				old_link = e
 				crawled.append(e)
-		old_node = good_link
-		self.path.append(old_node.node1)
+		self.path.append(old_link.node1)
+		i = 0
 		while self.chg>0:
-			for e in links:
-				if e.node1 == old_node.node2: #check second element of link
+			for e in links: #The problem lies here
+				if e.node1 == old_link.node2 and e not in crawled: #check second element of link
 					self.path.append(e.node1)
 					self.chg = e.charge_reduc(self.chg) #make sure the thought only goes so far
-					old_node = e #used for next iteration
-
+					old_link = e #used for next iteration
+					crawled.append(e)
+				elif links[-1] == e:
+					self.chg = self.chg - 1
 
 
 def add_link(one, two, charge_deduc=1):
@@ -47,12 +49,56 @@ pie = sg.words["noun"]["pie"]
 likes = sg.words["verb"]["to like"]
 eat = sg.words["verb"]["to eat"]
 food = sg.words["noun"]["food"]
+juice = sg.words["noun"]["juice"]
+cake = sg.words["noun"]["cake"]
+apple = sg.words["noun"]["apple"]
+banana = sg.words["noun"]["banana"]
+corn = sg.words["noun"]["corn"]
+milk = sg.words["noun"]["milk"]
+pear = sg.words["noun"]["pear"]
+water = sg.words["noun"]["water"]
+bread = sg.words["noun"]["bread"]
+goose = sg.words["noun"]["goose"]
+horse = sg.words["noun"]["horse"]
+kitten = sg.words["noun"]["kitten"]
+loaf = sg.words["noun"]["loaf"]
+owl = sg.words["noun"]["owl"]
+rabbit = sg.words["noun"]["rabbit"]
+robin = sg.words["noun"]["robin"]
+song = sg.words["noun"]["song"]
+rainstorm = sg.words["noun"]["rainstorm"]
+sound = sg.words["noun"]["sound"]
+stick = sg.words["noun"]["sound"]
+wood = sg.words["noun"]["wood"]
 add_link(pie, likes) #adding words that are linked together. This will become easer later on
 add_link(likes, eat)
+add_link(robin, stick, 2)
+add_link(stick, wood)
+add_link(wood, water, 2)
+add_link(eat, corn, 1.5)
+add_link(rainstorm, water, 1.5)
+add_link(sound, rainstorm)
+add_link(song, sound)
+add_link(horse, rainstorm)
 add_link(eat, food)
 add_link(food, pie)
+add_link(food, cake)
+add_link(apple, juice)
+add_link(apple, banana)
+add_link(juice, milk, 3)
+add_link(milk, water)
+add_link(pear, juice, 2)
+add_link(water, bread)
+add_link(bread, cake, 1.5)
+add_link(loaf, bread)
+add_link(bread, loaf)
+add_link(loaf, corn, 3)
+add_link(robin, song, 1.5)
+add_link(owl, robin, 2)
+add_link(apple, horse, 1.25)
+add_link(eat, apple)
 big_net = Network(nodes, links)
-big_net.follow(pie)
+big_net.follow(apple)
 for e in big_net.path:
 	print e.word
 sentence_sequence.append(big_net.path)
