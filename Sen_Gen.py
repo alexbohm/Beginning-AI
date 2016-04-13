b9d83545610cab2849592e71679912fd92ba7deb
Alex Bohm <aabohm7@gmail.com>
2016-04-13 18:24:34 -0500

    Made words read in without "Sen_Gen" class being called

--- Sen_Gen.py
+++ Sen_Gen.py
@@ -1,6 +1,5 @@
 from random import randrange
 import os
-words = {"noun":{}, "adjective":{}, "verb":{}, "adverb":{}, "conjunction":{}, "preposition":{}, "interjection":{}}
 class Word(object):
 	def __init__(self, word,w_type,forms = {}):
 		self.word = word
@@ -13,44 +12,18 @@ class Word(object):
 		else:
 			return "%s|%s\n" % (self.word, self.type)
 
+words = {"noun":{}, "adjective":{}, "verb":{}, "adverb":{}, "conjunction":{}, "preposition":{}, "interjection":{}}
+with open("%s/words.txt" % (os.getcwd()), "r") as f:
+	temp = f.read().splitlines()
+for word in temp:
+	temp2 = word.split("|")
+	if temp2[1]=="verb":
+		words["verb"][temp2[0]] = Word(temp2[0], "verb", eval(temp2[2]))
+	else:
+		words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1])
 class Sen_Gen(object):
 	def __init__(self, structure=[]):
 		self.structure = structure
-		self.path = os.getcwd()
-		with open("%s/words.txt" % (self.path), "r") as f:
-			temp = f.read().splitlines()
-		for word in temp:
-			temp2 = word.split("|")
-			if temp2[1]=="verb":
-				words["verb"][temp2[0]] = Word(temp2[0], "verb", eval(temp2[2]))
-			else:
-				words[temp2[1]][temp2[0]] = Word(temp2[0], temp2[1])
-	def add_word(self):
-		w_type = str(raw_input("Word Type: "))
-		while w_type not in words: #check if word type is valid
-			if w_type not in words:
-				print "invalid type"
-			w_type = str(raw_input("Word Type: "))
-		forms = {}
-		if w_type == "verb":
-			forms['infinitive'] = raw_input("Infinitive: ")
-			while forms['infinitive'] in words[w_type]: #check if word is valid
-				if forms['infinitive'] in words[w_type]:
-					print "Verb Aready in List"
-				forms['infinitive'] = raw_input("Infinitive: ")
-			forms['past'] = raw_input("Past Tense: ")
-			forms['present'] = raw_input("Present Tense: ")
-			forms['future'] = raw_input("Future Tense: ")
-			word = forms['infinitive']
-		else:
-			word = str(raw_input("Word: "))
-			while word in words[w_type]: #check if word is valid
-				if word in words[w_type]:
-					print "Aready in List"
-				word = str(raw_input("Word: "))
-		words[w_type][word] = Word(word, w_type, forms)
-		with open("%s/words.txt" % (self.path), "a") as f:
-			f.write(words[w_type][word].save())
 	def random_word(self, w_type): #find random word with inputed type
 		rand_num = randrange(0, len(words[w_type]))
 		i = 0
@@ -61,7 +34,7 @@ class Sen_Gen(object):
 	def get_verb(self, infinitive, tense="present"):
 			return words["verb"][infinitive].forms[tense]
 	def make_sen(self, amount=3, structure=[]):
-		if not len(structure) ==0:
+		if not len(structure) == 0:
 			self.structure = structure
 		senl = []
 		for i in range(amount):
@@ -69,16 +42,10 @@ class Sen_Gen(object):
 			for e in self.structure:
 				if e == "verb":
 					stc+= "%s " % (self.get_verb(self.random_word(e)))
-				elif e in self.words:
+				elif e in words:
 					stc+= "%s " % (self.random_word(e))
 				else:
 					stc+= "%s " % (e)
 			stc = stc[0:-1]
 			senl.append(stc)
 		return senl
-
-
-stru = ["adjective", "noun", "verb", "noun"] #, "adverb", "verb", "noun"
-simplesen = Sen_Gen(stru)
-print simplesen.make_sen(1)
-print simplesen.make_sen(1,["adjective", "noun", "verb", "adjective", "noun", "verb"])
