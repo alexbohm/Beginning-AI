1c3c45bded623a419491568b41c729a0fd69d1f6
Alex Bohm <aabohm7@gmail.com>
2016-04-22 16:39:27 -0500

    Added files via upload

--- Sen_Gen.py
+++ Sen_Gen.py
@@ -2,29 +2,30 @@ from random import randrange
 from os import getcwd
 import fileinput
 class Word(object):
-	def __init__(self, word,w_type,forms = {}):
+	def __init__(self, word,w_type,links = {}, forms = {}):
+		self.line = line
 		self.word = word
 		self.type = w_type
+		self.links = links
 		if w_type == "verb":
 			self.forms = forms
 words = {"noun":{}, "adjective":{}, "verb":{}, "adverb":{}, "conjunction":{}, "preposition":{}, "interjection":{}}
 def save():
 	with open("%s/words.txt" % (getcwd()), "w") as f:
 		for w_type in words:
-			if w_type in words:
-				for cla in words[w_type]:
-					if w_type =="verb":
-						f.write("%s|%s|%s\n" % (words[w_type][cla].word, words[w_type][cla].type, str(words[w_type][cla].forms)))
-					else:
-						f.write("%s|%s\n" % (words[w_type][cla].word, words[w_type][cla].type))
+			for cla in words[w_type]:
+				if w_type =="verb":
+					f.write("%s|%s|%s|%s\n" % (words[w_type][cla].word, words[w_type][cla].type, str(words[w_type][cla].links), str(words[w_type][cla].forms)))
+				else:
+					f.write("%s|%s|%s\n" % (words[w_type][cla].word, words[w_type][cla].type, str(words[w_type][cla].links)))
 
-for line in fileinput.input("%s/words.txt" % (getcwd())):
-	temp = line.strip("\n").split("|")
-	if temp[1]=="verb":
-		words["verb"][temp[0]] = Word(temp[0], "verb", eval(temp[2]))
-	else:
-		words[temp[1]][temp[0]] = Word(temp[0], temp[1])
-fileinput.close()
+with open("%s/words.txt" % (getcwd())) as f:
+	for line in f.read().splitlines():
+		temp = line.split("|")
+		if temp[1]=="verb":
+			words["verb"][temp[0]] = Word(temp[0], "verb", eval(temp[2]), eval(temp[3]))
+		else:
+			words[temp[1]][temp[0]] = Word(temp[0], temp[1], eval(temp[2]))
 
 class Sen_Gen(object):
 	def __init__(self, structure=[]):
